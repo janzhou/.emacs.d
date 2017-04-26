76f1ba3969bc50907647065f3455ffc1cc857b50
Jian Zhou <i@janzhou.org>
2017-04-26 16:51:30 -0400

    add magit key bind

--- init.el
+++ init.el
@@ -59,6 +59,10 @@
 
 (use-package magit
   :ensure t
+  :bind (("C-c l" . magit-log-head)
+         ("C-c c" . magit-commit)
+         ("C-c f" . magit-pull-from-upstream)
+         ("C-c p" . magit-push-current-to-upstream))
   :pin melpa-stable)
 
 (defun sh (name)
