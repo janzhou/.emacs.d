21f24ecc8e24d3389be9a04f6b45158ad793a7b5
Jian Zhou <i@janzhou.org>
2017-04-27 00:35:30 -0400

    config ensime

--- init.el
+++ init.el
@@ -55,6 +55,10 @@
 (use-package ensime
   :ensure t
   :demand
+  :config
+  (setq
+   ensime-sbt-command "/usr/bin/sbt"
+   sbt:program-name "/usr/bin/sbt.bat")
   :pin melpa-stable)
 
 (use-package magit
