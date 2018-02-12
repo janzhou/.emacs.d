e8cc4e18490f6bd789a029733db765b260cd9516
Jian Zhou <i@janzhou.org>
2018-02-12 11:50:45 -0500

    disable magit plugin

--- plugins/init.el
+++ plugins/init.el
@@ -22,9 +22,9 @@
 
 (load-file "~/.emacs.d/plugins/vim.el")
 (load-file "~/.emacs.d/plugins/scala.el")
-(load-file "~/.emacs.d/plugins/git.el")
+;; (load-file "~/.emacs.d/plugins/git.el")
 (load-file "~/.emacs.d/plugins/jekyll.el")
 (load-file "~/.emacs.d/plugins/shell.el")
 (load-file "~/.emacs.d/plugins/neotree.el")
-(load-file "~/.emacs.d/plugins/markdown.el")
+;; (load-file "~/.emacs.d/plugins/markdown.el")
 (load-file "~/.emacs.d/plugins/winum.el")
