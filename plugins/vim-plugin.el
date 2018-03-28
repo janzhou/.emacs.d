2947bdce9d2fb5455c00d634ccb4e4fb7cbef11a
Jian Zhou <i@janzhou.org>
2018-03-27 21:36:42 -0400

    add vim projectile bind

--- plugins/vim-plugin.el
+++ plugins/vim-plugin.el
@@ -19,6 +19,8 @@
      "ff" 'ido-find-file
      "ft" 'neotree-toggle
      "fw" 'save-buffer
+     "pf" 'projectile-find-file
+     "pi" 'projectile-invalidate-cache
      "pp" 'projectile-mode
      "pf" 'projectile-find-file
      "gl" 'vc-print-root-log
